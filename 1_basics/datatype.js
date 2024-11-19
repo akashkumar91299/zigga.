3d1488e3196d5270141f7ed044d65318f6e78a30
akashkumar91299 <ac737395@gmail.com>
2024-11-19 17:01:42 +0000

    datatype

---
+++ 1_basics/datatype.js
@@ -0,0 +1,12 @@
+// Number =>2 ki 53
+// Bigint
+// strintg=>""
+// Boolean =>true/false
+// null =>standalon value
+// undefine
+// object
+// synbol =>unique
+
+console.log(typeof undefined); //undefine
+console.log(typeof null); //object
+
