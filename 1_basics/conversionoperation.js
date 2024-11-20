faec5492b0c0e19ac9349486b1570d8935cac110
akashkumar91299 <ac737395@gmail.com>
2024-11-20 05:27:10 +0000

    conversionoperation

---
+++ 1_basics/conversionoperation.js
@@ -0,0 +1,72 @@
+let score="20afdgf"
+// console.log(typeof score);
+// console.log(typeof (score));
+
+
+
+let conversion =Number(score)
+// console.log(typeof conversion);
+// console.log( (conversion));
+
+
+let score1=null
+// console.log(typeof score1);
+// console.log(typeof (score1));
+
+
+
+let conversion1 =Number(score1)
+// console.log(typeof conversion1);
+// console.log( (conversion1));
+
+
+let score2=undefined
+// console.log(typeof score2);
+// console.log(typeof (score2));
+
+
+
+
+
+let conversion2 =Number(score2)
+// console.log(typeof conversion2);
+//console.log( (conversion2));
+
+// "22"=>22
+//"222aasd"=>NaN
+//true=> 1
+//false=>0
+
+
+//***************************OPERATION************************************
+ let value = 3
+  let negValue = -value
+ // console.log(negValue);
+
+
+//   console.log(2+2);
+//   console.log(5-3);
+//   console.log(20/2);
+//   console.log(10%2)
+//   console.log(2**4);
+
+
+
+let str1 = "Akash"
+let str2 = " Chauhan"
+let str3 = str1 + str2
+//console.log(str3);
+
+// console.log(1+"2");
+// console.log("1"+3);
+// console.log("1"+"5");
+// console.log(1+4+6+"9");
+// console.log("3"+5+7+8);
+// console.log(true);
+
+//console.log(+true);
+
+
+
+
+
