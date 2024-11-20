a840c5c71533eae3451f7ec904278a70dc64bb19
akashkumar91299 <ac737395@gmail.com>
2024-11-20 05:26:04 +0000

    conversion

---
+++ 1_basics/comparision.js
@@ -0,0 +1,28 @@
+// console.log(2>1);
+// console.log(2>=1);
+// console.log(2<1);
+// console.log(2<=1);
+// console.log(2!=1);
+
+/*
+console.log(null>0);
+console.log(null==0);
+console.log(null>=0);//null convert in to a number
+
+
+console.log(undefined>0);
+console.log(undefined==0);
+console.log(undefined>=0);
+
+avoid these type of comparision 
+because these type of amparision create a confusion
+*/
+
+
+
+
+
+
+
+
+
