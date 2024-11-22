3a499d88e831a43f24ac9c62c24745feeb25753c
akashkumar91299 <ac737395@gmail.com>
2024-11-22 17:44:58 +0000

    function

---
+++ 3_basics/function.js
@@ -0,0 +1,45 @@
+// console.log("A");
+// console.log("K");
+// console.log("A");
+// console.log("s");
+// console.log("H");
+
+
+function myName(){
+//     console.log("A");
+// console.log("K");
+// console.log("A");
+// console.log("s");
+// console.log("H");
+
+}
+
+
+//myName()
+
+
+// function addTwonumber(number1,number2){
+//    console.log(number1**number2);
+    
+
+// }
+
+
+
+function addTwonumber(number1,number2){
+   let result =(number1+number2);
+   console.log("Akash");
+
+    return result
+    console.log("Chauhan"); //this line not execute due tu before return
+    
+
+}
+const result =addTwonumber(2,5)
+console.log("result:",result);
+
+
+
+     
+ 
+ 
