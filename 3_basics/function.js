583a2e8cfe1c913349cc70e5d1ee5cb5659a7cce
akashkumar91299 <ac737395@gmail.com>
2024-11-23 15:58:38 +0000

    function

--- 3_basics/function.js
+++ 3_basics/function.js
@@ -5,14 +5,14 @@
 // console.log("H");
 
 
-function myName(){
-//     console.log("A");
-// console.log("K");
-// console.log("A");
-// console.log("s");
-// console.log("H");
+// function myName(){
+// //     console.log("A");
+// // console.log("K");
+// // console.log("A");
+// // console.log("s");
+// // console.log("H");
 
-}
+// }
 
 
 //myName()
@@ -26,20 +26,60 @@ function myName(){
 
 
 
-function addTwonumber(number1,number2){
-   let result =(number1+number2);
-   console.log("Akash");
+// function addTwonumber(number1,number2){
+//    let result =(number1+number2);
+//   // console.log("Akash");
 
-    return result
-    console.log("Chauhan"); //this line not execute due tu before return
+//     return result
+//     console.log("Chauhan"); //this line not execute due tu before return
     
 
+// }
+// const result =addTwonumber(2,5)
+// //console.log("result:",result);
+
+// function loginUserMessage(userName ="No")
+// {
+//    // if (userName==undefined){  **//it is also valid
+//    if(!userName){
+//        // console.log("Please enter username");
+//     return
+//     }
+//     return `${userName} just logged in`
+// }
+// //console.log(loginUserMessage("Akash Chauhan"));
+// //console.log(loginUserMessage("Akash Chauahn"));
+
+// function calculateCartPrice(...num1){
+//     return num1
+// }
+// //console.log(calculateCartPrice(200,300,500,600,700,800));
+
+
+// const user={
+// username :  "Akash chauhan",
+//price: 6000
+//}
+
+//function handleobject(anyobject){
+   // console.log(`username is ${anyobject.username} and price is ${anyobject.price}`)
+ //
+//handleobject(user)
+
+
+//handleobject({
+    //username: "xyz",
+  //  price: 399
+//})
+
+
+const myNewArray =[100,200,300,400]
+function returnvalue(gettingArray){
+    return gettingArray[2]
+    
 }
-const result =addTwonumber(2,5)
-console.log("result:",result);
-
-
-
+//console.log(returnvalue(myNewArray));
+console.log(returnvalue([1,2,3,4,5,6,7]));
      
  
  
