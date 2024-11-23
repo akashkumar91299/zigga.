1dcc847a447091a5c6300f5a68aa4d7ea00b2adf
akashkumar91299 <ac737395@gmail.com>
2024-11-23 16:15:30 +0000

    scope

---
+++ 3_basics/scope.js
@@ -0,0 +1,8 @@
+if(true){
+    let a=2
+    const b=4
+    var c=5
+}
+// console.log(a);
+// console.log(b);
+console.log(c); // here the value of c is not printed but var has no scope therefore the value of c is printed
