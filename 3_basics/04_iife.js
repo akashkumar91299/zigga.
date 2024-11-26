7cf8d9312ce9018161b9f8380f09af60932794eb
akashkumar91299 <ac737395@gmail.com>
2024-11-26 12:22:39 +0000

    iife

---
+++ 3_basics/04_iife.js
@@ -0,0 +1,18 @@
+// (function one(){
+//     console.log(`Hello Akash chauhan`);
+    
+// })();
+
+
+( () =>{
+    console.log(`Hello Akash chauhan`);
+    
+})(); 
+
+
+//   ***********  HOW TO PASS PARAMETER*************
+
+(function one(name){
+    console.log(`Hello ${name}`);
+    
+})("Mukul chauhan")
